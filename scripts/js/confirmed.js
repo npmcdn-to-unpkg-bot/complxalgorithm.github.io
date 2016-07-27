6a52ef8face8206d5bd03a2e821a30cba4af2b62
Stephen Sanders <scsandman6496@gmail.com>
2016-07-27 02:57:57 -0400

    major contact modal changes
    github pages unfortunately doesn’t allow php scripts to run

--- scripts/js/confirmed.js
+++
@@ -1,1 +0,0 @@
-jAlert('Thanks so much for the message! I\'ll make sure to get back to you soon. :),' 'It worked! :)');
--- scripts/js/contact_alert.js
+++
@@ -1,1 +0,0 @@
-jAlert('There was an error sending this message. Please try again!', 'Contact Error :(');
